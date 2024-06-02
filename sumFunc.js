8514ff66f90f945a501e9ba716034973f2f11f39
prodhanm <mustafizur.prodhan@gmail.com>
2024-06-02 19:44:46 +0600

    compiled ts type file to js

---
+++ sumFunc.js
@@ -0,0 +1,17 @@
+function total(a, b) {
+    return a + b;
+}
+
+console.log(total(10, 20));
+
+var total2 = function (a, b) {
+    return a + b;
+};
+
+console.log(total2(30, 20))
+
+var total3 = function (a, b) {
+    return a + b;
+};
+
+console.log(total3(40, 20));
---
+++ sumFunc.ts
@@ -0,0 +1,11 @@
+function total(a: number, b: number): number {
+    return a + b;
+}
+
+const total2 = function(a: number, b: number): number {
+    return a + b;
+}
+
+const total3 = (a,b) => {
+    return a + b;
+}
